3ae1ac9f7527f4290419afdaf54d531e6403394b
Abouzar <abouzarmoradian1@gmail.com>
2024-09-10 14:57:25 -0400

    added second screen

---
+++ DelegateProtocol/DelegateProtocol/SecondScreenVC.swift
@@ -0,0 +1,29 @@
+//
+//  SecondScreenVC.swift
+//  DelegateProtocol
+//
+//  Created by Abouzar Moradian on 9/10/24.
+//
+
+import UIKit
+
+class SecondScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
